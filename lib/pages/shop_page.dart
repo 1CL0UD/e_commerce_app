3b35e20e62ab57771050df2223d35542751d28ac
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-03-31 06:07:08 +0700

    fixed card size

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -25,56 +25,76 @@ class ShopPage extends StatelessWidget {
             child: GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
+                childAspectRatio: 0.8,
               ),
               padding: const EdgeInsets.fromLTRB(8, 4, 8, 12),
               itemCount: 12,
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 1,
-                        blurRadius: 8,
-                      ),
-                    ],
-                  ),
-                  child: IntrinsicHeight(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(4),
-                          child: Image.network(
-                            'https://img.hellofresh.com/w_1920,q_auto,f_auto,c_limit,fl_lossy/c_fill,f_auto,fl_lossy,h_432,q_auto/hellofresh_s3/image/643ee2286638513a700c8d1c-6ce17906.jpg',
-                          ),
-                        ),
-                        const Text(
-                          'Product A',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const Text(
-                          '1kg',
-                          style: TextStyle(fontWeight: FontWeight.w400),
-                        ),
-                        const Text(
-                          '%2',
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                return const ProductCard();
               },
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 8,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4),
+            child: Image.network(
+              'https://img.hellofresh.com/w_1920,q_auto,f_auto,c_limit,fl_lossy/c_fill,f_auto,fl_lossy,h_432,q_auto/hellofresh_s3/image/643ee2286638513a700c8d1c-6ce17906.jpg',
+            ),
+          ),
+          const Text(
+            'Product A',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            '1kg',
+            style: TextStyle(fontWeight: FontWeight.w400),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                '#2',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('ATC');
+                  },
+                  child: Icon(Icons.add))
+            ],
           ),
         ],
       ),
