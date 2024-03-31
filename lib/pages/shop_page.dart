762d8ff154ddcb0822ee32528e38f7a2d8c634af
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-03-31 07:07:27 +0700

    shop page done

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,37 +1,151 @@
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatelessWidget {
-  const ShopPage({Key? key}) : super(key: key);
+  ShopPage({Key? key}) : super(key: key);
+  final List<Map<String, dynamic>> products = [
+    {
+      'image':
+          'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'name': 'Product 1',
+      'price': '\$100',
+      'description': 'Description of Product 1',
+    },
+    {
+      'image':
+          'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'name': 'Product 2',
+      'price': '\$150',
+      'description': 'Description of Product 2',
+    },
+    {
+      'image':
+          'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'name': 'Product 3',
+      'price': '\$50',
+      'description': 'Description of Product 3',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(16, 12, 16, 6),
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
             child: TextField(
               decoration: const InputDecoration(
                 hintText: 'Search a product',
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(),
               ),
-              onChanged: (value) {
-                // Implement your search logic here
-              },
+              onChanged: (value) {},
             ),
           ),
           Expanded(
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.8,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(12, 8, 12, 2),
+              child: ListView(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(8, 8, 8, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Exclusive Offer",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "See All",
+                          style: TextStyle(fontSize: 18, color: Colors.green),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 230,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: products.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ProductCard(
+                          image: products[index]['image'],
+                          name: products[index]['name'],
+                          price: products[index]['price'],
+                          description: products[index]['description'],
+                        );
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(8, 8, 8, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Best Selling",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "See All",
+                          style: TextStyle(fontSize: 18, color: Colors.green),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 230,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: products.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ProductCard(
+                          image: products[index]['image'],
+                          name: products[index]['name'],
+                          price: products[index]['price'],
+                          description: products[index]['description'],
+                        );
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(8, 8, 8, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Groceries",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "See All",
+                          style: TextStyle(fontSize: 18, color: Colors.green),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 230,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: products.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ProductCard(
+                          image: products[index]['image'],
+                          name: products[index]['name'],
+                          price: products[index]['price'],
+                          description: products[index]['description'],
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-              padding: const EdgeInsets.fromLTRB(8, 4, 8, 12),
-              itemCount: 12,
-              itemBuilder: (BuildContext context, int index) {
-                return const ProductCard();
-              },
             ),
           ),
         ],
@@ -41,60 +155,91 @@ class ShopPage extends StatelessWidget {
 }
 
 class ProductCard extends StatelessWidget {
+  final String image;
+  final String name;
+  final String price;
+  final String description;
+
   const ProductCard({
     super.key,
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.description,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(8),
-      padding: const EdgeInsets.all(8),
+      width: MediaQuery.of(context).size.width * 0.44,
+      margin: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
         color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 1,
-            blurRadius: 8,
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
           ),
         ],
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(4),
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
             child: Image.network(
-              'https://img.hellofresh.com/w_1920,q_auto,f_auto,c_limit,fl_lossy/c_fill,f_auto,fl_lossy,h_432,q_auto/hellofresh_s3/image/643ee2286638513a700c8d1c-6ce17906.jpg',
+              image,
+              width: double.infinity,
+              height: 100,
+              fit: BoxFit.cover,
             ),
           ),
-          const Text(
-            'Product A',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  price,
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        description,
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        print("ATC");
+                      },
+                      child: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          Text(
-            '1kg',
-            style: TextStyle(fontWeight: FontWeight.w400),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                '#2',
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print('ATC');
-                  },
-                  child: Icon(Icons.add))
-            ],
           ),
         ],
       ),
