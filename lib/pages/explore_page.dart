ff666f4973d67885eb1c8fd12728cc9fef8d1e67
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-03 05:43:46 +0700

    cards done

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -5,8 +5,35 @@ class ExplorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Explore Page')),
+    return Scaffold(
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemBuilder: (context, index) => Center(
+                child: Container(
+                  margin: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.green[300]!.withOpacity(0.4),
+                    border:
+                        Border.all(color: Colors.green[300]!.withOpacity(1.0)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(32.0),
+                          child: Image.network(
+                            'https://clipart-library.com/image_gallery2/Fruit.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const Text("123"),
+                    ],
+                  ),
+                ),
+              )),
     );
   }
 }
