6fc0a732fd74561794c9374520aa2ad8ab8b76d7
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-03 06:37:53 +0700

    added new search component

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/components/search_bar.dart';
 import 'package:flutter/material.dart';
 
 class ExplorePage extends StatelessWidget {
@@ -6,35 +7,47 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                mainAxisSpacing: 16, crossAxisSpacing: 4, crossAxisCount: 2),
-            itemBuilder: (context, index) => Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green[300]!.withOpacity(0.4),
-                      border: Border.all(
-                          color: Colors.green[300]!.withOpacity(1.0)),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(32.0),
-                            child: Image.asset(
-                              'assets/images/fruit_images/fruit (1).png',
-                              fit: BoxFit.cover,
-                            ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            child: SearchBarComponent(),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+              child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 4,
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) => Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.green[300]!.withOpacity(0.4),
+                            border: Border.all(
+                                color: Colors.green[300]!.withOpacity(1.0)),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(32.0),
+                                  child: Image.asset(
+                                    'assets/images/fruit_images/fruit (1).png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const Text("123"),
+                            ],
                           ),
                         ),
-                        const Text("123"),
-                      ],
-                    ),
-                  ),
-                )),
+                      )),
+            ),
+          ),
+        ],
       ),
     );
   }
