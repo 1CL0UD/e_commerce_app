79e4aedf14dd21cb4f40d2fa19346691ea626798
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-03 06:03:20 +0700

    fixed spacing and layout

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -6,34 +6,36 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemBuilder: (context, index) => Center(
-                child: Container(
-                  margin: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.green[300]!.withOpacity(0.4),
-                    border:
-                        Border.all(color: Colors.green[300]!.withOpacity(1.0)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(32.0),
-                          child: Image.asset(
-                            'assets/images/fruit_images/fruit (1).png',
-                            fit: BoxFit.cover,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 16, crossAxisSpacing: 4, crossAxisCount: 2),
+            itemBuilder: (context, index) => Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green[300]!.withOpacity(0.4),
+                      border: Border.all(
+                          color: Colors.green[300]!.withOpacity(1.0)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: Image.asset(
+                              'assets/images/fruit_images/fruit (1).png',
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                      const Text("123"),
-                    ],
+                        const Text("123"),
+                      ],
+                    ),
                   ),
-                ),
-              )),
+                )),
+      ),
     );
   }
 }
