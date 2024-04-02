29543400f2c09a214eab263420ea69a3fd9a2d00
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-04-03 05:47:08 +0700

    changed card shadow to border

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -176,14 +176,7 @@ class ProductCard extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
         color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
+        border: Border.all(color: Colors.grey.withOpacity(0.4), width: 1.5),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
